86b154620ca7b93b503edc81a6f94c95238000b8
djfuck <daniel@ahrari.dev>
2021-02-14 18:48:46 +0200

    Revert "Code for YouTube watch URL processing"
    This reverts commit 3732a618c6f94ba1b509dd36abc5be1c0f0967ac.

--- Program.fs
+++ Program.fs
@@ -1,20 +1,6 @@
-﻿open HtmlAgilityPack
-open System.Text.RegularExpressions
-open System.Web
-
-let IsYouTubeWatchLink urlString = 
-    Regex.IsMatch(urlString, @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$")
-
-let GetTitleFromBody (htmlDoc: HtmlDocument) =
-    let node = htmlDoc.DocumentNode.SelectSingleNode("/html/body/title")
-    if node = null then "error" else HttpUtility.HtmlDecode(node.InnerText)
+﻿open System
 
 [<EntryPoint>]
 let main argv =
-    let testUrl = "https://www.youtube.com/watch?v=6-IRnDofg6Y&list=RD6-IRnDofg6Y&start_radio=1"
-
-    if IsYouTubeWatchLink(testUrl) then
-        let htmlDoc = (new HtmlWeb()).Load(testUrl)
-        System.Console.WriteLine(GetTitleFromBody(htmlDoc))
-
+    printfn "bessawissa"
     0

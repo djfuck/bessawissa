de4a5bc3c02dfab66526ff75d36662fe6d2c8e17
djfuck <76790441+djfuck@users.noreply.github.com>
2021-02-14 18:57:50 +0200

    Youtube stuff
    closes #5 closes #6

--- Program.fs
+++ Program.fs
@@ -1,6 +1,20 @@
-﻿open System
+open HtmlAgilityPack
+open System.Text.RegularExpressions
+open System.Web
+
+let IsYouTubeWatchLink urlString = 
+    Regex.IsMatch(urlString, @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$")
+
+let GetTitleFromBody (htmlDoc: HtmlDocument) =
+    let node = htmlDoc.DocumentNode.SelectSingleNode("/html/body/title")
+    if node = null then "error" else HttpUtility.HtmlDecode(node.InnerText)
 
 [<EntryPoint>]
 let main argv =
-    printfn "bessawissa"
+    let testUrl = "https://www.youtube.com/watch?v=6-IRnDofg6Y&list=RD6-IRnDofg6Y&start_radio=1"
+
+    if IsYouTubeWatchLink(testUrl) then
+        let htmlDoc = (new HtmlWeb()).Load(testUrl)
+        System.Console.WriteLine(GetTitleFromBody(htmlDoc))
+
     0

57902bf991e472a63eb8ac2a089408f6a8678a70
djfuck <daniel@ahrari.dev>
2021-02-15 09:34:49 +0200

    IRC functionality stub

--- Program.fs
+++ Program.fs
@@ -1,9 +1,11 @@
 open HtmlAgilityPack
 open System.Text.RegularExpressions
 open System.Web
+open SimpleIRCLib
 
 let IsYouTubeWatchLink urlString = 
-    Regex.IsMatch(urlString, @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$")
+    let ismatch = Regex.IsMatch(urlString, @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$")
+    ismatch
 
 let GetTitleFromBody (htmlDoc: HtmlDocument) =
     let node = htmlDoc.DocumentNode.SelectSingleNode("/html/body/title")
@@ -11,10 +13,31 @@ let GetTitleFromBody (htmlDoc: HtmlDocument) =
 
 [<EntryPoint>]
 let main argv =
-    let testUrl = "https://www.youtube.com/watch?v=6-IRnDofg6Y&list=RD6-IRnDofg6Y&start_radio=1"
 
-    if IsYouTubeWatchLink(testUrl) then
-        let htmlDoc = (new HtmlWeb()).Load(testUrl)
-        System.Console.WriteLine(GetTitleFromBody(htmlDoc))
+    if argv.Length <> 3 then
+        printfn "%s" "Invalid amount of arguments given. Correct syntax:\n[SERVER] [NICKNAME] [CHANNEL]"
+        1
+    else
 
+    let irc = new SimpleIRC()
+    irc.SetupIrc(argv.[0], argv.[1], argv.[2]) |> ignore
+
+    let ReceivedEventHandler (rcvArgs : IrcReceivedEventArgs) =
+        printfn "%s" rcvArgs.Message
+        if (rcvArgs.Channel = argv.[2] && IsYouTubeWatchLink(rcvArgs.Message) = true) then
+            let htmlDoc = (new HtmlWeb()).Load(rcvArgs.Message)
+            irc.SendMessageToChannel(GetTitleFromBody(htmlDoc), argv.[2]) |> ignore
+
+    irc.IrcClient.OnMessageReceived.Add(ReceivedEventHandler)
+
+    if (irc.StartClient() = false) then
+        printfn "%s" "Couldn't start IRC client."
+        1
+    else
+
+    while (irc.IsClientRunning()) do
+        if System.Console.ReadLine() = "quit" then
+            printfn "%s" "Stopping IRC client..."
+            irc.StopClient() |> ignore
+            printfn "%s" "Stopped."
     0
